369673f24124a66a8c42826d76bca74c2e4c9770
Drew Fisher <awfisher@wpi.edu>
2021-10-07 21:13:17 -0400

    add demo scatterplot

---
+++ src/components/plots/scatterplot.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { scaleLinear } from 'd3-scale';
+	interface ScatterData {
+		points: point[];
+	}
+	interface point {
+		x: number;
+		y: number;
+	}
+	export let data: ScatterData;
+	export let minPoint: point;
+	export let maxPoint: point;
+	export let xTickCount: number;
+	export let yTickCount: number;
+    export let precision: number = 2;
+
+	let svg: SVGElement;
+	$: width = 100;
+	$: height = 100;
+
+	let xTicks = linspace(minPoint.x, maxPoint.x, xTickCount);
+	let yTicks = linspace(minPoint.y, maxPoint.y, yTickCount);
+
+	const padding = { top: 50, right: 50, bottom: 50, left: 50 };
+	$: xScale = scaleLinear()
+		.domain([minPoint.x, maxPoint.x])
+		.range([padding.left, width - padding.right]);
+
+	$: yScale = scaleLinear()
+		.domain([minPoint.y, maxPoint.y])
+		.range([height - padding.bottom, padding.top]);
+	onMount(resize);
+	function resize() {
+		({ width, height } = svg.getBoundingClientRect());
+		// xTicks = linspace(minPoint.x, maxPoint.x, xTickCount);
+		// yTicks = linspace(minPoint.y, maxPoint.y, yTickCount);
+	}
+
+	function linspace(start: number, stop: number, steps: number): number[] {
+		var arr = [];
+		var step = (stop - start) / (steps - 1);
+		for (var i = 0; i < steps; i++) {
+			arr.push(start + step * i);
+		}
+		return arr;
+	}
+</script>
+
+<svelte:window on:resize={resize} />
+
+<svg bind:this={svg}>
+	<g class="axis y-axis">
+		{#each yTicks as tick}
+			<g class="tick tick-{tick.toPrecision(precision)}" transform="translate(0, {yScale(tick)})">
+				<line x1={padding.left} x2={xScale(maxPoint.x)} />
+				<text x={padding.left - 8} y="+4">{tick}</text>
+			</g>
+		{/each}
+	</g>
+
+	<g class="axis x-axis">
+		{#each xTicks as tick}
+			<g class="tick" transform="translate({xScale(tick)},0)">
+				<line y1={yScale(0)} y2={yScale(maxPoint.y)} />
+				<text y={height - padding.bottom + 16}>{tick.toPrecision(precision)}</text>
+			</g>
+		{/each}
+	</g>
+
+	<!-- data -->
+	{#each data.points as point}
+		<circle cx={xScale(point.x)} cy={yScale(point.y)} r="5" />
+	{/each}
+</svg>
+
+<style>
+	svg {
+		height: 100%;
+		width: 100%;
+	}
+	circle {
+		fill: red;
+		fill-opacity: 0.6;
+		stroke: rgba(0, 0, 0, 0.5);
+	}
+	.tick line {
+		stroke: #222;
+		stroke-dasharray: 2;
+	}
+	text {
+		font-size: 12px;
+		fill: #999;
+	}
+	.x-axis text {
+		text-anchor: middle;
+        writing-mode: vertical-rl;
+	}
+	.y-axis text {
+		text-anchor: end;
+	}
+</style>
--- src/routes/servo.svelte
+++ src/routes/servo.svelte
@@ -1,26 +1,37 @@
 <script lang='ts'>
+    import Scatterplot from "../components/plots/scatterplot.svelte";
     import ServoControl from "../components/servoControl.svelte";
-        
-    </script>
-    <div id="container">
-        <div>
-            <h1>Servo Tester</h1>
-            <p>This demo uses WebBluetooth to send angles to a servo</p>
-        </div>
-    
-        <ServoControl/>
+</script>
+<div id="container">
+    <div>
+        <h1>Servo Tester</h1>
+        <p>This demo uses WebBluetooth to send angles to a servo</p>
     </div>
-    <style>
-        #container {
-            max-width: 100vw;
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-        }
-        #container > * {
-            padding: 1rem;
-        }
-        :global(body) {
-            margin: 0;
-        }
-    </style>
+
+    <ServoControl/>
+    <Scatterplot data={{points: [
+        {x: 10, y: 10},
+        {x: 20, y: 60},
+        {x: 40, y: 10},
+    ]}}
+    minPoint={{x: 0, y: 0}}
+    maxPoint={{x: 60, y: 100}}
+    xTickCount={16}
+    yTickCount={11}
+    ></Scatterplot>
+</div>
+
+<style>
+    #container {
+        max-width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+    #container > * {
+        padding: 1rem;
+    }
+    :global(body) {
+        margin: 0;
+    }
+</style>
