d28e256cc6e9fd54b84690d01db0223f5762d8b7
Brian Fay <bcfay@wpi.edu>
2022-04-13 14:11:15 -0400

    StartNotifications again

--- src/lib/components/controls.svelte
+++ src/lib/components/controls.svelte
@@ -65,7 +65,7 @@
 				});
 			};
 			await char.readValue();
-			//await char.startNotifications();
+			await char.startNotifications();
 		});
 		// 	return Promise.all([
 		//         service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e").then(handleCharacteristicLogString),
